a46319f1671e4075fad7d474c15e4fa474d2bf95
Kole Gasior <jkole822@gmail.com>
2020-12-21 12:08:59 -0500

    create inquirer prompt

--- app.js
+++ app.js
@@ -10,6 +10,173 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const generateRandomId = () => {
+	const characters = [
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+		"K",
+		"L",
+		"M",
+		"N",
+		"O",
+		"P",
+		"Q",
+		"R",
+		"S",
+		"T",
+		"U",
+		"V",
+		"W",
+		"X",
+		"Y",
+		"Z",
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+	];
+
+	let id = "";
+
+	for (let i = 0; i < 8; i++) {
+		const index = Math.floor(Math.random() * characters.length);
+		id += characters[index];
+	}
+
+	return id;
+};
+
+const promptAddMember = () => {
+	return inquirer.prompt([
+		{
+			type: "list",
+			name: "role",
+			message: "Employee Role:",
+			choices: ["Manager", "Engineer", "Intern"],
+		},
+		{
+			type: "input",
+			name: "name",
+			message: "Employee Name:",
+			validate(input) {
+				const regex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|\d/;
+
+				if (!input) {
+					return console.log("\nName required");
+				} else if (input.match(regex)) {
+					return console.log("\nName cannot include numbers or symbols");
+				}
+
+				return true;
+			},
+		},
+		{
+			type: "input",
+			name: "id",
+			message: "Employee ID:",
+			default: generateRandomId(),
+			validate(input) {
+				const regex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+
+				if (input.match(regex)) {
+					return console.log("\nID cannot include symbols");
+				}
+
+				return true;
+			},
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "Employee email:",
+			// Uses regex from https://emailregex.com/ for email validation if an email is provided
+			validate(input) {
+				const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+				if (!input) {
+					return console.log("\nEmail required");
+				} else if (!input.match(regex)) {
+					return console.log("\nInvalid email");
+				}
+
+				return true;
+			},
+		},
+		{
+			type: "input",
+			name: "officenumber",
+			message: "Office Number:",
+			when: answers => answers.role === "Manager",
+			validate(input) {
+				const regex = /\D/;
+
+				if (!input) {
+					return console.log("\nOffice number required");
+				} else if (input.match(regex)) {
+					return console.log("\nOffice number can only include numbers");
+				}
+
+				return true;
+			},
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "GitHub Username:",
+			when: answers => answers.role === "Engineer",
+			validate(input) {
+				if (!input) {
+					return console.log("\nGitHub username required");
+				}
+
+				return true;
+			},
+		},
+		{
+			type: "input",
+			name: "school",
+			message: "School:",
+			when: answers => answers.role === "Intern",
+			validate(input) {
+				const regex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|\d/;
+
+				if (!input) {
+					return console.log("\nSchool required");
+				} else if (input.match(regex)) {
+					return console.log("\nSchool cannot include numbers or symbols");
+				}
+
+				return true;
+			},
+		},
+	]);
+};
+
+const init = async () => {
+	try {
+		const answers = await promptAddMember();
+
+		console.log(answers);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
